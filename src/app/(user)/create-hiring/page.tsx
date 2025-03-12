c8b17fa0df853536cfc4fddf5d4e10a4453c71ea
Ngô Khôi <khoingo2003@gmail.com>
2025-03-12 11:36:40 +0700

    Refine seed data

--- src/app/(user)/create-hiring/page.tsx
+++ src/app/(user)/create-hiring/page.tsx
@@ -12,7 +12,7 @@ import { useEffect, useRef, useState } from "react";
 const CreatePage = () => {
     const customerLogin = useAppSelector((state) => state.userSlice.user);
 
- 
+
 
 
     const { addToast } = useToast();
@@ -79,7 +79,7 @@ const CreatePage = () => {
         setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     };
 
- 
+
 
 
     const handleCategorySelect = (category: string) => {
@@ -123,26 +123,101 @@ const CreatePage = () => {
             } catch (error) {
                 console.error(error);
                 const bookListFailCase = [
-                    { id: 1, name: "English 1", img: "https://www.womenlivingwellafter50.com.au/wp-content/uploads/2023/08/Looking-for-Jane-1.jpg" },
-                    { id: 2, name: "Creative Design", img: "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg" },
-                    { id: 3, name: "Fantasy Woods", img: "https://marketplace.canva.com/EAFfSnGl7II/2/0/1003w/canva-elegant-dark-woods-fantasy-photo-book-cover-vAt8PH1CmqQ.jpg" },
-                    { id: 4, name: "Summer Sunset", img: "https://thumbs.dreamstime.com/b/l-summer-sunset-portrait-landscape-nature-illustration-river-mountain-jungle-sunrise-birds-flying-water-book-cover-vector-333100990.jpg" },
-                    { id: 5, name: "Fatal Exchange", img: "https://smallbluedog.com/wp-content/uploads/2014/10/FatalExchange_Cover-small.jpg" },
-                    { id: 6, name: "Purple Sky", img: "https://marketplace.canva.com/EAGKkPbakT4/1/0/1003w/canva-purple-sky-book-cover-UOKyRwxSEZQ.jpg" },
-                    { id: 7, name: "Graphic Wonders", img: "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg" },
-                    { id: 8, name: "Mystic Night", img: "https://marketplace.canva.com/EAGKkPbakT4/1/0/1003w/canva-purple-sky-book-cover-UOKyRwxSEZQ.jpg" },
-                    { id: 9, name: "Jungle Secrets", img: "https://thumbs.dreamstime.com/b/l-summer-sunset-portrait-landscape-nature-illustration-river-mountain-jungle-sunrise-birds-flying-water-book-cover-vector-333100990.jpg" },
-                    { id: 10, name: "Lost in Time", img: "https://smallbluedog.com/wp-content/uploads/2014/10/FatalExchange_Cover-small.jpg" },
-                    { id: 11, name: "Design Patterns", img: "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg" },
-                    { id: 12, name: "Dark Realms", img: "https://marketplace.canva.com/EAFfSnGl7II/2/0/1003w/canva-elegant-dark-woods-fantasy-photo-book-cover-vAt8PH1CmqQ.jpg" },
-                    { id: 13, name: "Eternal Forest", img: "https://thumbs.dreamstime.com/b/l-summer-sunset-portrait-landscape-nature-illustration-river-mountain-jungle-sunrise-birds-flying-water-book-cover-vector-333100990.jpg" },
-                    { id: 14, name: "Echoes of War", img: "https://smallbluedog.com/wp-content/uploads/2014/10/FatalExchange_Cover-small.jpg" },
-                    { id: 15, name: "Sky fall Chronicles", img: "https://marketplace.canva.com/EAGKkPbakT4/1/0/1003w/canva-purple-sky-book-cover-UOKyRwxSEZQ.jpg" },
-                    { id: 16, name: "Urban Legends", img: "https://marketplace.canva.com/EAFfSnGl7II/2/0/1003w/canva-elegant-dark-woods-fantasy-photo-book-cover-vAt8PH1CmqQ.jpg" },
-                    { id: 17, name: "Whispers in the Wind", img: "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg" },
-                    { id: 18, name: "The Forgotten Path", img: "https://smallbluedog.com/wp-content/uploads/2014/10/FatalExchange_Cover-small.jpg" },
-                    { id: 19, name: "Shadows and Light", img: "https://marketplace.canva.com/EAGKkPbakT4/1/0/1003w/canva-purple-sky-book-cover-UOKyRwxSEZQ.jpg" },
-                    { id: 20, name: "The Last Voyage", img: "https://thumbs.dreamstime.com/b/l-summer-sunset-portrait-landscape-nature-illustration-river-mountain-jungle-sunrise-birds-flying-water-book-cover-vector-333100990.jpg" }
+                    {
+                        id: 1,
+                        name: "Global Success 5",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583474/edusource/Global-Success-5.webp"
+                    },
+                    {
+                        id: 2,
+                        name: "Global Success 3",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583470/edusource/Global-Success-3.jpg"
+                    },
+                    {
+                        id: 3,
+                        name: "Family and Friends 2",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583455/edusource/Family-And-Friends-2.png"
+                    },
+                    {
+                        id: 4,
+                        name: "Global Success 1",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736587391/edusource/Global-Success-1.jpg"
+                    },
+                    {
+                        id: 5,
+                        name: "I Learn Smart Start 1",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736586096/edusource/I-Learn-Smart-Start-1.jpg"
+                    },
+                    {
+                        id: 6,
+                        name: "Global Success 2",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583472/edusource/Global-Success-2.jpg"
+                    },
+                    {
+                        id: 7,
+                        name: "Family and Friends 1",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583455/edusource/Family-And-Friends-1.png"
+                    },
+                    {
+                        id: 8,
+                        name: "Family and Friends 3",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583455/edusource/Family-And-Friends-3.png"
+                    },
+                    {
+                        id: 9,
+                        name: "I Learn Smart Start 5",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583438/edusource/I-Learn-Smart-Start-5.jpg"
+                    },
+                    {
+                        id: 10,
+                        name: "Family and Friends 5",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583454/edusource/Family-And-Friends-5.jpg"
+                    },
+                    {
+                        id: 11,
+                        name: "Global Success 4",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583471/edusource/Global-Success-4.jpg"
+                    },
+                    {
+                        id: 12,
+                        name: "Family and Friends 4",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583456/edusource/Family-And-Friends-4.png"
+                    },
+                    {
+                        id: 13,
+                        name: "I Learn Smart Start 3",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583439/edusource/I-Learn-Smart-Start-3.png"
+                    },
+                    {
+                        id: 14,
+                        name: "I Learn Smart Start 2",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583438/edusource/I-Learn-Smart-Start-2.jpg"
+                    },
+                    {
+                        id: 15,
+                        name: "I Learn Smart Start 4",
+                        img: "https://res.cloudinary.com/dc4eascme/image/upload/v1736583438/edusource/I-Learn-Smart-Start-4.jpg"
+                    },
+                    { id: 21, name: "English 1", img: "https://www.womenlivingwellafter50.com.au/wp-content/uploads/2023/08/Looking-for-Jane-1.jpg" },
+                    { id: 22, name: "Creative Design", img: "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg" },
+                    { id: 23, name: "Fantasy Woods", img: "https://marketplace.canva.com/EAFfSnGl7II/2/0/1003w/canva-elegant-dark-woods-fantasy-photo-book-cover-vAt8PH1CmqQ.jpg" },
+                    { id: 24, name: "Summer Sunset", img: "https://thumbs.dreamstime.com/b/l-summer-sunset-portrait-landscape-nature-illustration-river-mountain-jungle-sunrise-birds-flying-water-book-cover-vector-333100990.jpg" },
+                    { id: 25, name: "Fatal Exchange", img: "https://smallbluedog.com/wp-content/uploads/2014/10/FatalExchange_Cover-small.jpg" },
+                    { id: 26, name: "Purple Sky", img: "https://marketplace.canva.com/EAGKkPbakT4/1/0/1003w/canva-purple-sky-book-cover-UOKyRwxSEZQ.jpg" },
+                    { id: 27, name: "Graphic Wonders", img: "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg" },
+                    { id: 28, name: "Mystic Night", img: "https://marketplace.canva.com/EAGKkPbakT4/1/0/1003w/canva-purple-sky-book-cover-UOKyRwxSEZQ.jpg" },
+                    { id: 29, name: "Jungle Secrets", img: "https://thumbs.dreamstime.com/b/l-summer-sunset-portrait-landscape-nature-illustration-river-mountain-jungle-sunrise-birds-flying-water-book-cover-vector-333100990.jpg" },
+                    { id: 40, name: "Lost in Time", img: "https://smallbluedog.com/wp-content/uploads/2014/10/FatalExchange_Cover-small.jpg" },
+                    { id: 31, name: "Design Patterns", img: "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg" },
+                    { id: 32, name: "Dark Realms", img: "https://marketplace.canva.com/EAFfSnGl7II/2/0/1003w/canva-elegant-dark-woods-fantasy-photo-book-cover-vAt8PH1CmqQ.jpg" },
+                    { id: 33, name: "Eternal Forest", img: "https://thumbs.dreamstime.com/b/l-summer-sunset-portrait-landscape-nature-illustration-river-mountain-jungle-sunrise-birds-flying-water-book-cover-vector-333100990.jpg" },
+                    { id: 34, name: "Echoes of War", img: "https://smallbluedog.com/wp-content/uploads/2014/10/FatalExchange_Cover-small.jpg" },
+                    { id: 35, name: "Sky fall Chronicles", img: "https://marketplace.canva.com/EAGKkPbakT4/1/0/1003w/canva-purple-sky-book-cover-UOKyRwxSEZQ.jpg" },
+                    { id: 36, name: "Urban Legends", img: "https://marketplace.canva.com/EAFfSnGl7II/2/0/1003w/canva-elegant-dark-woods-fantasy-photo-book-cover-vAt8PH1CmqQ.jpg" },
+                    { id: 37, name: "Whispers in the Wind", img: "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg" },
+                    { id: 38, name: "The Forgotten Path", img: "https://smallbluedog.com/wp-content/uploads/2014/10/FatalExchange_Cover-small.jpg" },
+                    { id: 39, name: "Shadows and Light", img: "https://marketplace.canva.com/EAGKkPbakT4/1/0/1003w/canva-purple-sky-book-cover-UOKyRwxSEZQ.jpg" },
+                    { id: 30, name: "The Last Voyage", img: "https://thumbs.dreamstime.com/b/l-summer-sunset-portrait-landscape-nature-illustration-river-mountain-jungle-sunrise-birds-flying-water-book-cover-vector-333100990.jpg" }
                 ];
 
                 setBookList(bookListFailCase);
@@ -193,7 +268,7 @@ const CreatePage = () => {
                         required
                     />
 
-             
+
                     <div className="relative" ref={categoryDropdownRef}>
                         <input
                             type="text"
@@ -280,7 +355,7 @@ const CreatePage = () => {
 
 
                     {/* Book Selection */}
-                  
+
                     <div className="relative" ref={bookDropdownRef}>
                         <input
                             type="text"
@@ -363,7 +438,25 @@ const categoryImageMap: Record<string, string> = {
     "Adventure": "https://img.freepik.com/free-vector/walking-through-forest-adventure_24877-76269.jpg",
     "Mystery": "https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/the-mystery-of-ourselves-cameron-gray.jpg",
     "Fantasy": "https://www.tallengestore.com/cdn/shop/products/Fantasy_Art_-_Woman_Warrior_With_Tiger_c0829677-def5-4c2e-ab7e-fc9e6345c85d.jpg?v=1568967247",
+    "Science Fiction": "https://media.wired.com/photos/5f4ee313bb023d54b55e4e29/master/pass/sci-fi-last-lines.jpg",
+    "Horror": "https://render.fineartamerica.com/images/rendered/medium/print/8/5.5/break/images-medium-5/castle-in-the-sky-bob-orsillo.jpg",
+    "Thriller": "https://i1.sndcdn.com/artworks-2EgNieCOutUfspte-YlVksw-t500x500.jpg",
+    "Romance": "https://w0.peakpx.com/wallpaper/598/440/HD-wallpaper-love-is-in-the-air-love-theme.jpg",
+    "Psychology": "https://www.simplypsychology.org/wp-content/uploads/psychology.jpeg",
+    "Poetry": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIm_qLdHkkcal1leonDXqwLAwaeyxu9tC9pA&s",
+    "Classic": "https://media.slidesgo.com/storage/33479731/vintage-papyrus-minitheme1680506366.jpg",
+    "Dystopian": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbSk_ENtLHN9M3KSqrnAVfpKq0ladNJmxWmw&s",
+    "Utopian": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxEMEY4AKjOG_Rb48cX_FU1Un2d3fVOvsZJA&s",
+    "Fairy Tale": "https://m.media-amazon.com/images/S/pv-target-images/82aff91a387ad745a6480e2af852a3f950e86bb9c9663c5858ba57c3397cfe1e.jpg",
+    "Education": "https://vnis.edu.vn/wp-content/uploads/2024/04/edu-trends.png",
+    "Travel": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVmlM4cOwkqa95XuYml6OFgbn4gYHok96R1Q&s",
+    "Business": "https://imageio.forbes.com/specials-images/imageserve/5fca87f3ce4ca55e8985a10a/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds",
+    "Politics": "https://www.voicesofyouth.org/sites/voy/files/images/2019-01/politics3.jpg",
+    "Sports": "https://assets.goal.com/images/v3/getty-2153876891/crop/MM5DINZYGQ5DENRZGE5G433XMU5DCMZRHI3DINI=/GettyImages-2153876891.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+    "Music": "https://moises.ai/_next/image/?url=https%3A%2F%2Fstorage.googleapis.com%2Fmoises-cms%2Fhow_to_reading_sheet_music_image_338d99b137%2Fhow_to_reading_sheet_music_image_338d99b137.jpg&w=1920&q=75",
+    "Art": "https://art.rtistiq.com/en-us/_next/image?url=https%3A%2F%2Fd28jbe41jq1wak.cloudfront.net%2FBlogsImages%2FPop_Art_Compressed_638237807745595223.jpg&w=1920&q=75",
 };
+
 
 const bookImgMap: Record<string, string> = {
     "English 1": "https://source.unsplash.com/150x150/?abc,alphabet",
